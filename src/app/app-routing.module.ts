aa60ed072f1d2e62de3dd480df1f13853ff7e0dd
Arian Provenzano <aprovenzano@ms.local>
2019-11-14 09:28:42 -0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LogInComponent } from './components/log-in/log-in.component';
+import { ListProductComponent } from './components/list-product/list-product.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login', component: LogInComponent},
+  {path: 'products', component: ListProductComponent},
+  {path: '', redirectTo: '/login', pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
