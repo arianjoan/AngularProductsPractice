fd236651914fca2fe9e9e9a078ed4b0f9e57d243
Arian Provenzano <aprovenzano@ms.local>
2019-11-14 12:45:52 -0300

    paginator added

--- src/app/components/list-product/list-product.component.ts
+++ src/app/components/list-product/list-product.component.ts
@@ -10,12 +10,23 @@ import { Product } from 'src/app/models/product';
 export class ListProductComponent implements OnInit {
 
   products : Product[];
+  qProducts : Number;
 
   constructor(private productService : ProductsService) { }
 
   ngOnInit() {
     this.productService.getProducts(0).subscribe((products) => {
       this.products = products.items;
+      this.qProducts = products.total / 50;
+      console.log(this.qProducts);
+    })
+  }
+
+  changePage(event){
+    this.productService.getProducts(event - 1).subscribe((products) => {
+      this.products = products.items;
+      this.qProducts = products.total / 50;
+      console.log(this.qProducts);
     })
   }
 
